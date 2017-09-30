8e36fb97d8d7ee80618e55d8f58ee0ca3e029530
Andre Leinio <andreleinio@hotmail.com>
2017-09-30 03:41:13 -0300

    DD v0.5

--- hydrogen/js/newPost.js
+++ hydrogen/js/newPost.js
@@ -1,12 +1,15 @@
 function queNaoVaiDarOQue() {
 
-    var item = document.createElement("item");
-    var ab = document.createElement("animate-box");
+    var item = document.createElement("div");
+    item.className = "item";
+    var ab = document.createElement("div");
+    ab.className = "animate-box bounceIn animated";
 
     
     var html = '<a href="images/img_1.jpg" class="image-popup fh5co-board-img" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, eos?"><img src="images/img_1.jpg" alt="Free HTML5 Bootstrap template"></a>' +
     '<div class="fh5co-desc" > AAAAAAAAAAAAAAAAAA, consectetur adipisicing elit.Explicabo, eos  </div >';
     ab.innerHTML = html;
     item.appendChild(ab);
-    document.getElementById("fh5co-board").appendChild(item);
+
+    salvattore.prependElements(document.getElementById("fh5co-board"),[item]);
 }
