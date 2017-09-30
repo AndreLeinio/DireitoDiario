b405e889c138c2fcfb0f6e6ab8c54cce991b143c
Andre Leinio <andreleinio@hotmail.com>
2017-09-30 04:22:11 -0300

    Post with fields

--- hydrogen/js/newPost.js
+++ hydrogen/js/newPost.js
@@ -1,13 +1,18 @@
 function queNaoVaiDarOQue() {
 
+    var link = document.getElementById("imageLink").value;
+    var texto = document.getElementById("depoimento").value;
+    var tags = document.getElementById("tag").value;
+    var nome = document.getElementById("Name").innerText;
+
     var item = document.createElement("div");
     item.className = "item";
     var ab = document.createElement("div");
     ab.className = "animate-box bounceIn animated";
 
     
-    var html = '<a href="images/img_1.jpg" class="image-popup fh5co-board-img" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, eos?"><img src="images/img_1.jpg" alt="Free HTML5 Bootstrap template"></a>' +
-    '<div class="fh5co-desc" > AAAAAAAAAAAAAAAAAA, consectetur adipisicing elit.Explicabo, eos  </div >';
+    var html = '<a href="'+link+'" class="image-popup fh5co-board-img"><img src="'+ link +'" alt="Free HTML5 Bootstrap template"></a>' +
+        '<div class="fh5co-desc" > <b>' + nome + '</b><br>' + texto +'<br><sub>'+tags.replace(","," ")+'</sub>  </div >';
     ab.innerHTML = html;
     item.appendChild(ab);
 
